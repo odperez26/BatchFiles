e4c4a0bba77c800f6e4936112262ff0cc99bd002
daniel43 <odperez@uach.mx>
2026-02-06 18:15:16 -0600

    Agregada opción de multiplicar
    Se agrega opción de multiplicar y se quitan los :goto y se ponen exit /b

--- Funciones.bat
+++ Funciones.bat
@@ -1,17 +1,23 @@
 @echo off
 
 set /p Opc=Ingrese 1 para sumar, 2 para restar, 3 para multiplicar, 4 para dividir
-if %Opc%==1 (call :sumar 5 3) else (call :restar 5 3) 
-goto :eof
+if %Opc%==1 (call :sumar 5 3) else (if %Opc%==2 (call :restar 5 3) else (call :multiplicar 5 3)) 
+exit /b
 
 :sumar
 set /a resultado= %1 + %2
 echo suma=%resultado%
 pause
-goto :eof
+exit /b
 
 :restar
 set /a resultado= %1 - %2
 echo resta=%resultado%
 pause
-goto :eof
+exit /b
+
+:multiplicar
+set /a resultado= %1 * %2
+echo multiplicacion=%resultado%
+pause
+exit /b
